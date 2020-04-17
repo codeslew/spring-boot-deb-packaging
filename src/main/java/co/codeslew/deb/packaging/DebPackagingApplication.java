3ce4b4eb7670b01f7505bc5b9d078a9de55ef157
Marshall Chabanga <marshallchabanga@gmail.com>
2020-04-17 21:35:09 +0200

    initial commit

---
+++ src/main/java/co/codeslew/deb/packaging/DebPackagingApplication.java
@@ -0,0 +1,13 @@
+package co.codeslew.deb.packaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebPackagingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DebPackagingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/codeslew/deb/packaging/DebPackagingApplicationTests.java
@@ -0,0 +1,13 @@
+package co.codeslew.deb.packaging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebPackagingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
